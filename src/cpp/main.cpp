fe38e6b6b23ccbadffc288ceddbbfb9854d528fc
Sigmund <sigmund.slang@gmail.com>
2017-11-30 10:37:06 +0100

    startup cpp

---
+++ src/cpp/main.cpp
@@ -0,0 +1,23 @@
+
+
+void forwardEuler(){
+  //TODO construct forwardEuler
+}
+
+
+void backwardEuler(){
+  //TODO construct bakcwardEuler
+}
+
+
+void crankNicolson(){
+  //TODO construct Crank-Nicolson
+  //My estimate is that it has to test the theta factor
+  //and then call either bwdEuler or fwdEuler.
+}
+
+
+
+int main(int argc, char *argv[]){
+  //TODO initialize the entire system
+}
