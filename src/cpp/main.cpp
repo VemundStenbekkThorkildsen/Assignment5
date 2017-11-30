a659f70110a2c1a032ff7481a2f7bd8cb21830dc
Sigmund <sigmund.slang@gmail.com>
2017-11-30 12:29:40 +0100

    main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -9,6 +9,9 @@ void crankNicolson(){
 }
 
 
+
+
+
 void bwdEuler(int n, int tsteps, double delta_x, double alpha)
 {
   double a, b, c;
@@ -19,7 +22,7 @@ void bwdEuler(int n, int tsteps, double delta_x, double alpha)
   a = c = - alpha;
   b = 1 + 2*alpha;
   for (int t = 1; t <= tsteps; t++) {
-    tridag(a, b, c, y, u, n+1);
+    tridag(n, y, a, b, c, v);
     u(0) = u(n) = 0;
     for (int i = 0; i <= n; i++) {
       y(i) = u(i);
@@ -30,27 +33,23 @@ void bwdEuler(int n, int tsteps, double delta_x, double alpha)
 }
 
 
+void tridiag(int n, vec& y, double a, double b, double c, vec& v)
+{
+    vec b_tilde(n);
+    vec y_tilde(n);
 
-
-
-void tridiag(double a, double b, double c, double *y, double *u, int n){
-    double *d_tilde = new double[n];
-    double *b_tilde = new double[n];
-    d_tilde[0] = d_array[0];
-    b_tilde[0] = b_array[0]/d_array[0];
-
+    b_tilde(0) = b;
+    y_tilde(0) = y(0)/b;
 
     for (int i = 1; i < n; i++){
-        d_tilde[i] = d_array[i] - (c_array[i-1]*a_array[i-1])/d_tilde[i-1];
-        b_tilde[i] = b_array[i] - (b_tilde[i-1]*c_array[i-1])/d_tilde[i-1];
+        b_tilde(i) = b - (c*a)/b_tilde(i-1);
+        y_tilde(i) = y(i) - (b_tilde(i-1)*c/b_tilde(i-1);
     }
-
-    v[n-1] = b_tilde[n-1]/d_tilde[n-1];
-    for (int k = n-2; k>= 0; k--){
-        v[k] = (b_tilde[k] - c_array[k]*v[k+1])/d_tilde[k];
+    v(n-1) = y(n-1)/b_tilde(n-1);
+    for (int k = n-2; k>=0; k--){
+        v(k) = (y_tilde(k) + v(k+1))/b_tilde(k);
     }
-
-
+}
 
 
 int main(int argc, char *argv[]){
