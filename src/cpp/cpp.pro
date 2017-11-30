626a92e65fa8aca2305c38b37606b959d7f9e0dd
VemundStenbekkThorkildsen <31346944+VemundStenbekkThorkildsen@users.noreply.github.com>
2017-11-30 17:29:27 +0100

    cpp

--- src/cpp/cpp.pro
+++ src/cpp/cpp.pro
@@ -4,3 +4,10 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp
+
+LIBS += -LC:\Armadillo\newblas -llibblas
+LIBS += -LC:\Armadillo\newblas -lliblapack
+
+
+INCLUDEPATH += C:\Armadillo\include
+DEPENDPATH += C:\Armadillo\include
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -11,29 +11,6 @@ void crankNicolson(){
   //and then call either bwdEuler or fwdEuler.
 }
 
-
-
-void bwdEuler(int n, int tsteps, double delta_x, double alpha)
-{
-  double a, b, c;
-  vec u(n+1);
-  vec y(n+1);
-  y(n) = u(n) = 1;
-  y(0) = u(0) = 0;
-  a = c = - alpha;
-  b = 1 + 2*alpha;
-  for (int t = 1; t <= tsteps; t++) {
-    tridag(n+1, y, a, b, c, u);
-    u(0) = u(n) = 0;
-    for (int i = 0; i <= n; i++) {
-      y(i) = u(i);
-    }
-    //y.print()
-  }
-  y.print()
-}
-
-
 void tridiag(int n, vec& y, double a, double b, double c, vec& u)
 {
     vec b_tilde(n);
@@ -44,7 +21,7 @@ void tridiag(int n, vec& y, double a, double b, double c, vec& u)
 
     for (int i = 1; i < n; i++){
         b_tilde(i) = b - (c*a)/b_tilde(i-1);
-        y_tilde(i) = y(i) - (b_tilde(i-1)*c/b_tilde(i-1);
+        y_tilde(i) = y(i) - (b_tilde(i-1)*c/b_tilde(i-1));
     }
     u(n-1) = y(n-1)/b_tilde(n-1);
     for (int k = n-2; k>=0; k--){
@@ -52,24 +29,44 @@ void tridiag(int n, vec& y, double a, double b, double c, vec& u)
     }
 }
 
+void bwdEuler(int n, int tsteps, double delta_x, double alpha)
+{
+  double a, b, c;
+  vec u = zeros(n+1);
+  vec y = zeros(n+1);
+  y(n) = u(n) = 1;
+  y(0) = u(0) = 0;
+  a = c = - alpha;
+  b = 1 + 2*alpha;
+  for (int t = 1; t <= tsteps; t++) {
+    tridiag(n+1, y, a, b, c, u);
+    u(0) = u(n) = 0;
+    for (int i = 0; i <= n; i++) {
+      y(i) = u(i);
+    }
+    //y.print()
+  }
+  y.print();
+}
+
 
 int main(int argc, char *argv[])
 {
-int n, int t_max, int tsteps;
-double dx, double dt;
-if (argv >= 4){
-  n = argv[1];
-  t_max = argv[2];
-  dx = argv[3];
+int n, t_max, tsteps;
+double dx, dt;
+if (argc > 3){
+  n = atoi(argv[1]);
+  t_max = atoi(argv[2]);
+  dx = atof(argv[3]);
 } else {
   cout << "Bad usage" << endl;
 }
 
-dt = dx^2/4;
+dt = (dx*dx)/4;
 tsteps = t_max/dt;
 double alpha = dt/(dx*dx);
 
-bwdEuler(n, tsteps, dx, alpha)
+bwdEuler(n, tsteps, dx, alpha);
 
 
 
