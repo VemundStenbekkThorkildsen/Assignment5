b0c8a39828055f0c300923359babeb77e35ef874
Sigmund <sigmund.slang@gmail.com>
2017-12-01 13:46:02 +0100

    working bwd

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <fstream>
 #include "armadillo"
 
-=======
-<<<<<<< HEAD
-#include "iostream"
-#include "fstream"
-#include <armadillo>
-=======
->>>>>>> 83e5cb9b54a028660a5c7e614766f64b55fe0e92
-#include <iostream>
-#include <fstream>
-#include "armadillo"
->>>>>>> df276b3a5c75f3467631eeded4174a12bcb55e78
 
 using namespace arma;
 using namespace std;
@@ -29,21 +17,26 @@ void writeToFile(vec& u, string filename)
 
 void tridiag(int n, vec& y, double a, double b, double c, vec& u)
 {
-    vec b_tilde(n);
-    vec y_tilde(n);
+    vec b_tilde = zeros(n);
+    vec y_tilde = zeros(n);
 
     b_tilde(0) = b;
-    y_tilde(0) = y(0)/b;
+    y_tilde(0) = y(0);
 
     for (int i = 1; i < n; i++){
         b_tilde(i) = b - (c*a)/b_tilde(i-1);
-        y_tilde(i) = y(i) - (b_tilde(i-1)*c/b_tilde(i-1));
+        y_tilde(i) = y(i) - (y_tilde(i-1)*c/b_tilde(i-1));
     }
-    u(n-1) = y(n-1)/b_tilde(n-1);
-    for (int k = n-2; k>=0; k--){
-        u(k) = (y_tilde(k) + u(k+1))/b_tilde(k);
+    //print
+
+    //b_t * u_n-1 + c*u_n = y_t_n-1
+    u(n-1) = (y(n-1)-c*u(n))/b_tilde(n-1);
+    for (int k = n-2; k>0; k--){
+        u(k) = (y_tilde(k) - c*u(k+1))/b_tilde(k);
     }
+
 }
+
 
 void fwdEuler(int n, int tsteps, double alpha){
 
@@ -55,14 +48,12 @@ void fwdEuler(int n, int tsteps, double alpha){
     for(int t = 1; t <= tsteps; t++){
         u(0) = u(n) = 0;
         tridiag(n,y,a,b,c,u);
-        for(int i = 0; i<=n;i++){
+        for(int i = 0; i<=n; i++){
             y(i) = u(i);
         }
     }
-
-
-
 }
+
 
 void bwdEuler(int n, int tsteps, double alpha)
 {
@@ -74,8 +65,9 @@ void bwdEuler(int n, int tsteps, double alpha)
   a = c = - alpha;
   b = 1 + 2*alpha;
   for (int t = 1; t <= tsteps; t++) {
-    tridiag(n+1, y, a, b, c, u);
-    u(0) = u(n) = 0;
+    tridiag(n, y, a, b, c, u);
+    u(0) = 0;
+    u(n) = 1;
     for (int i = 0; i <= n; i++) {
       y(i) = u(i);
     }
@@ -92,55 +84,28 @@ void crankNicolson(){
 }
 
 
-int main(int argc, char *argv[]) //n,tmax,dx
+int main(int argc, char *argv[]) //n,tmax,tsteps
 {
 
 int n, t_max, tsteps;
 double dx, dt;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> df276b3a5c75f3467631eeded4174a12bcb55e78
 
->>>>>>> 83e5cb9b54a028660a5c7e614766f64b55fe0e92
 if (argc == 4){
   n = atoi(argv[1]);
   t_max = atoi(argv[2]);
-  dx = atof(argv[3]);
-}
-else {
+  tsteps = atof(argv[3]);
+} else {
   cout << "Bad usage" << endl;
   exit(1);
 }
 
-dt = (dx*dx)/4;
-<<<<<<< HEAD
-
-=======
-=======
->>>>>>> 83e5cb9b54a028660a5c7e614766f64b55fe0e92
->>>>>>> df276b3a5c75f3467631eeded4174a12bcb55e78
-
-dt = (dx*dx)/4;
-<<<<<<< HEAD
-//t_max = 10;
-<<<<<<< HEAD
-tsteps = t_max/dt;
+dx = 1.0/n;
+dt = ((double) t_max)/((double) tsteps);
 double alpha = dt/(dx*dx);
 
 bwdEuler(n, tsteps, alpha);
-cout << y_tilde;
-=======
-=======
-dt = (dx*dx)/4;
->>>>>>> 83e5cb9b54a028660a5c7e614766f64b55fe0e92
-tsteps = t_max/dt;
-double alpha = dt/(dx*dx);
+//fwdEuler(n, tsteps, alpha);
 
-//bwdEuler(n, tsteps, alpha);
-fwdEuler(n,tsteps,alpha);
 
->>>>>>> df276b3a5c75f3467631eeded4174a12bcb55e78
 }
