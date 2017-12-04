d766e46e75368ccf3cc7a94d593d3db70d342773
Sigmund <sigmund.slang@gmail.com>
2017-12-04 16:45:36 +0100

    more code

--- src/cpp/fwdeuler2.cpp
+++ src/cpp/fwdeuler2.cpp
@@ -14,11 +14,16 @@ void fwd2Euler(int n, int tsteps, double dt, double dx){
   }
 
   for (int t = 1; t <= tsteps; t++) {
-    for(int i=1; i < n; i++){
-      for(int j=1; j < n; j++){
-
+    for(int i=0; i <=n; i++){
+      for(int j=0; j <=n; j++){
+        if(i==0 || i==n){
+          continue
+        } else if(j==0){
+            u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j+1) - 4*u0(i,j));
+        } else if(j==n){
+            u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j-1) - 4*u0(i,j));
+        }
         u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j+1) + u0(i,j-1) - 4*u0(i,j));
-
       }
     }
     u0 = u;
