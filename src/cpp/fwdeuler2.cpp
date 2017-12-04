36679a737cbb6580f1eaf5d8ff014e89efb66e0b
sanderwl <sanderl@student.matnat.uio.no>
2017-12-04 17:01:14 +0100

    uild

--- src/cpp/fwdeuler2.cpp
+++ src/cpp/fwdeuler2.cpp
@@ -16,14 +16,31 @@ void fwd2Euler(int n, int tsteps, double dt, double dx){
   for (int t = 1; t <= tsteps; t++) {
     for(int i=0; i <=n; i++){
       for(int j=0; j <=n; j++){
-        if(i==0 || i==n){
-          continue
-        } else if(j==0){
+
+          if(j == 0){
+              u(i,j) = 0;
+          }
+          else if(j == n){
+              u(i,j) = 1;
+          }
+          else if(i == 0){
+              u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j+1) - 4*u0(i,j));
+          }
+          else if(i == n){
+              u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j-1) - 4*u0(i,j));
+          }
+          else{
+              u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j+1) + u0(i,j-1) - 4*u0(i,j));
+          }
+        /*
+        if(j==0 && i != 0 && i != n){
             u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j+1) - 4*u0(i,j));
-        } else if(j==n){
+        } else if(j==n && i != 0 && i != n){
             u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j-1) - 4*u0(i,j));
+        }else if(j != n && j!= 0 && i != n && i != 0){
+            u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j+1) + u0(i,j-1) - 4*u0(i,j));
         }
-        u(i,j) = u0(i,j) + alpha*(u0(i+1,j) + u0(i-1,j) + u0(i,j+1) + u0(i,j-1) - 4*u0(i,j));
+        */
       }
     }
     u0 = u;
