f13ecd794313721d0ee7ff1071c9db3f64272618
Sigmund <sigmund.slang@gmail.com>
2017-12-06 13:29:45 +0100

    fixup

--- src/cpp/fwdeuler.cpp
+++ src/cpp/fwdeuler.cpp
@@ -1,26 +1,15 @@
 #include "methods.h"
 
-void fwdEuler(int n, int tsteps, double alpha, double ro, double k, double c)
+void fwdEuler(int n, int tsteps, double alpha)
 {
   vec unew = zeros(n+1);
   vec u = zeros(n+1);
   u(0) = unew(0) = 0;
   u(n) = unew(n) = 1;
 
-  double Q;
-
   for (int t = 1; t <= tsteps; t++) {
     for (int i = 1; i < n; i++) {
-        if(i<=20){
-            Q = 1.4; //myW/m^3
-        }
-        else if(i>20 && i<=40){
-            Q = 0.35; //myW/m^3
-        }
-        else if(i>40){
-            Q = 0.05; //myW/m^3
-        }
-      unew(i) = (k*(alpha * u(i-1) + (1 - 2*alpha) * u(i) + alpha * u(i+1)) + Q)/(ro*c);
+      unew(i) = (alpha * u(i-1) + (1 - 2*alpha) * u(i) + alpha * u(i+1));
     }
     u = unew;
   }
